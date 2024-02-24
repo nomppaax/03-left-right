166d00e7b480c587ef3bfd3c64008bb98055d600
Noomi <kykkanenn@gmail.com>
2024-02-24 11:16:12 +0200

    15-Laskurin arvo propsien avulla komponentille Display

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   const increaseByOne = () => setCounter(counter + 1);
-
   const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
-
+      <Display counter={counter} />
       <button onClick={increaseByOne}>plus</button>
-
       <button onClick={setToZero}>zero</button>
     </div>
   );
