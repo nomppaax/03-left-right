37efef315f67f5376fa567e0ac8e1ba03cb477e4
Noomi <kykkanenn@gmail.com>
2024-02-26 14:05:46 +0200

    18-Komponenttien refaktorointi ja tehtävä valmis

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-const Display = (props) => {
-  return <div>{props.counter}</div>;
-};
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>;
-};
+const Display = ({ counter }) => <div>{counter}</div>;
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 const App = () => {
   const [counter, setCounter] = useState(0);
 
