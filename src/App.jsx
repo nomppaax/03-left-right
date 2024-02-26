68418af67eb84be501760afd1e02b617e0a3c9f0
Noomi <kykkanenn@gmail.com>
2024-02-26 14:03:56 +0200

    17-Konsoliin tulee kommentti, kun nappia painetaan.

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,26 @@ const Button = (props) => {
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1);
+  console.log("rendering with counter value", counter);
 
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+    setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
+  };
 
   return (
     <div>
       <Display counter={counter} />
-
       <Button handleClick={increaseByOne} text="plus" />
       <Button handleClick={setToZero} text="zero" />
       <Button handleClick={decreaseByOne} text="minus" />
