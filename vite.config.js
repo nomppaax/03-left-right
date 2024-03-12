adea73a9a7d35b415775fedfa14cbaeb06cfc3ff
Noomi <kykkanenn@gmail.com>
2024-03-12 12:17:57 +0000

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/03-left-right/", // Korjaa tämä polku vastaamaan GitHub Pages -polkua
+  base: "/03-left-right/",
   plugins: [react()],
 });
